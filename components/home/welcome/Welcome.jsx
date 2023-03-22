4cc05e7452a469bc16f37c4f37f7b47141b22e1b
Jared Gutkin <jgutkinsam@gmail.com>
2023-03-21 23:02:41 -0500

    added search field

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -11,12 +11,45 @@ import { useRouter } from 'expo-router';
 
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants';
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"]
+
 const Welcome = () => {
   return (
     <View>
       <View style={styles.container}>
           <Text style={styles.userName}>Hello MIMI</Text>
           <Text style={styles.welcomeMessage}>Find your Perfect Job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={()=>{}}>
+          <Image 
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+
+          />
+        </TouchableOpacity>
+
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList 
+          data={jobTypes}
+          renderItem={({item})=>(
+            <TouchableOpacity>
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
       </View>
     </View>
   )
