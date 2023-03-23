46888ad2eed490e7418fe9b58694b04093d1b1ce
Jared Gutkin <jgutkinsam@gmail.com>
2023-03-22 20:31:59 -0500

    set up api functionality

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -7,12 +7,18 @@ import { useRouter } from 'expo-router';
 import styles from './popularjobs.style';
 import { COLORS, SIZES } from '../../../constants';
 import PopularJobCard from '../../common/cards/popular/PopularJobCard';
+import useFetch from '../../../hook/useFetch'
 import { isLoading } from 'expo-font';
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+
+  const { data, isLoading, error } = useFetch
+  ('search', {
+    query: 'React Developer',
+    num_pages: 1
+  })
+
 
   return (
     <View style={styles.container}>
